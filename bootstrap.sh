914e0c07edc68236ec4750229565eb87c660a5a4
Lukas Sembera <semberal@gmail.com>
2015-10-05 22:43:02 +0200

    Big refactor, ansible is not necessary on the host machine anymore.

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# sudo apt-get update
+sudo apt-get install -y ansible
+ansible-playbook -i localhost, -c local -s /home/vagrant/ansible-playbooks/playbook.yaml
+echo
+echo
+echo
+echo "******************************************"
+echo "** INSTALLATION COMPLETED SUCCESSFULLY! **"
+echo "******************************************"
+echo
+echo "Everything has been configured, please follow the following steps to connect to the Vertica database:"
+echo "* vagrant ssh"
+echo "* sudo su dbadmin"
+echo "* vsql"
+echo "OR"
+echo "* adminTools"
