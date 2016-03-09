891ec36f9f371010b84beaba2669f53accc4bb9d
Lukas Sembera <semberal@gmail.com>
2016-03-09 22:49:28 +0100

    Success message improved

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,17 @@ set -e
 # sudo apt-get update
 sudo apt-get install -y ansible
 ansible-playbook -i localhost, -c local -s /home/vagrant/ansible-playbooks/playbook.yaml
-echo
-echo
-echo
 echo "******************************************"
 echo "** INSTALLATION COMPLETED SUCCESSFULLY! **"
 echo "******************************************"
-echo
-echo "Everything has been configured, please follow these steps to connect to the Vertica database:"
-echo "* vagrant ssh"
-echo "* sudo su dbadmin"
-echo "* vsql"
-echo "OR"
-echo "* adminTools"
+echo "Everything has been configured, you can now connect to the Vertica database:"
+echo "* Command line"
+echo "*** vagrant ssh"
+echo "*** sudo su dbadmin"
+echo "*** vsql or adminTools"
+echo "* JDBC"
+echo "*** Host: localhost:5433"
+echo "*** Database: default"
+echo "*** Username: dbadmin"
+echo "*** Password: (empty)"
+echo "*** JDBC URI: jdbc:vertica://localhost:5433/default"
